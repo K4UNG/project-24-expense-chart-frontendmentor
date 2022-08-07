1378c955966e262457a2f38647e490cce6e089d0
K4UNG <kgzinhein.my@gmail.com>
2022-08-08 00:01:46 +0630

    HTML summary senmatic fix

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,16 +6,15 @@ export default function Body() {
     <div className={styles.body}>
       <h2 className={styles.body__title}>Spending - Last 7 days</h2>
       <Chart/>
-      {/* one missing */}
       <div className={styles.body__line} />
       <h3 className={styles.body__total}>Total this month</h3>
-      <summary className={styles.summary}>
+      <div className={styles.summary}>
         <h4 className={styles.summary__amount}>$478.33</h4>
         <div className={styles.summary__stats}>
           <h5 className={styles.summary__percent}>+2.4%</h5>
           <span>from last month</span>
         </div>
-      </summary>
+      </div>
     </div>
   );
 }
